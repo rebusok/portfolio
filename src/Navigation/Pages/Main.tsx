e749aed3c6c20b09c949b45a02fda45fc2451ea6
Yuriy <rebusok@yandex.ru>
2021-05-07 02:53:15 +0300

    finish skils

--- src/Navigation/Pages/Main.tsx
+++ src/Navigation/Pages/Main.tsx
@@ -1,16 +1,23 @@
-import React, {lazy} from 'react';
+import React, {lazy, Suspense} from 'react';
 import Skills from "./Skills/Skills";
 import Jobs from "./Jobs/Jobs";
 import Employ from "./Employ/Employ";
 import Contact from "./Contact/Contact";
 import Footer from "./Footer/Footer";
 const AvatarComponent = lazy(() => import("./Presentation/Presentation"));
+const renderLoader = () => <p>Loading</p>;
 const Main = () => {
     return (
         <React.Fragment>
-            <AvatarComponent/>
-            <Skills/>
-            <Jobs/>
+            <Suspense fallback={renderLoader()}>
+                <AvatarComponent />
+            </Suspense>
+            <Suspense fallback={renderLoader()}>
+                <Skills/>
+            </Suspense>
+            <Suspense fallback={renderLoader()}>
+                <Jobs/>
+            </Suspense>
             <Employ/>
             <Contact title={'Contact'}/>
             <Footer/>
