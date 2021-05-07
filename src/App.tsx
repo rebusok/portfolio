84f6e4c05560fd7a23427e49c37603f601eeab9e
Yuriy <rebusok@yandex.ru>
2021-05-07 03:52:42 +0300

    finish skils

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Layout from './components/layout/Layout';
 function App() {
     return (
         <div className={'app'}>
+
             <HashRouter>
                 <Layout/>
             </HashRouter>
